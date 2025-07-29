d2e73b66efc652e21b3895d3b9e42404bba89a31
david vega <david.vega01@usc.edu.co>
2025-07-28 20:50:13 -0500

    refactor category

---
+++ src/muebles/data/categorias.js
@@ -0,0 +1,17 @@
+export const categorias = [
+  {
+    id: 1,
+    image: "categoria1",
+    description: "Oficina",
+  },
+    {
+    id: 2,
+    image: "categoria2",
+    description: "Hogar",
+  },
+    {
+    id: 3,
+    image: "categoria3",
+    description: "Cocina",
+  },
+];
